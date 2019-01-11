af43f22f22b64e79832d2e5b73f39605029f6826
Aishwary Ujjwal <akaishwary477@gmail.com>
2019-01-11 16:39:35 +0530

    new

--- app/install/config.php
+++ app/install/config.php
@@ -2,7 +2,7 @@
 
 error_reporting(E_ERROR);
 date_default_timezone_set('Asia/Calcutta');
-ini_set("error_log", "../data/punch-in.log");
+ini_set("error_log", "../data/app-install.log");
 define('CURRENT_PATH',dirname(__FILE__));
 define('CLIENT_APP_PATH',realpath(dirname(__FILE__)."/..")."/");
 define('CLIENT_APP_CONFIG_PATH',realpath(dirname(__FILE__)."/..")."/config/");
